cc0b44230b6caa6c80a9b660858776e4d44d3697
Jack-Corbett <31970629+Jack-Corbett@users.noreply.github.com>
2018-07-24 22:04:15 +0100

    Update fetchName.java

--- src/fetchName.java
+++ src/fetchName.java
@@ -21,33 +21,32 @@ public class fetchName {
 		URL url = new URL(address);
 		
 		//Connects to the site and starts the input stream
-	    URLConnection urlConnection = url.openConnection();
-	    BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	    	URLConnection urlConnection = url.openConnection();
+	    	BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         
-        String str;
-        //We are looking for the title tag as this is followed by the name
-        String search = "<title>";
-        //Store position pointers of the name
-        Integer nameStart;
-        Integer nameEnd;
-        //Boolean flag when name found stop searching the file
-        Boolean found = false;
-        
-        while (((str = in.readLine()) != null) & !found) {
-            str.toString();
-            //Check it that line has the title tag
-            if (str.toLowerCase().contains(search.toLowerCase())) {
-            		//Find the name and save it
-                nameStart = str.indexOf('>') + 1;
-            		nameEnd = str.indexOf('|') - 1;
-            		name = str.substring(nameStart, nameEnd);
-            		//Trigger found flag
-            		found = true;
-            }
-        }
-        in.close();
-		
-        System.out.println("The name of this user is: " + name);
-	}
+		String str;
+		//We are looking for the title tag as this is followed by the name
+		String search = "<title>";
+		//Store position pointers of the name
+		Integer nameStart;
+		Integer nameEnd;
+		//Boolean flag when name found stop searching the file
+		Boolean found = false;
 
+		while (((str = in.readLine()) != null) & !found) {
+		    str.toString();
+		    //Check it that line has the title tag
+		    if (str.toLowerCase().contains(search.toLowerCase())) {
+				//Find the name and save it
+			nameStart = str.indexOf('>') + 1;
+				nameEnd = str.indexOf('|') - 1;
+				name = str.substring(nameStart, nameEnd);
+				//Trigger found flag
+				found = true;
+		    }
+		}
+		in.close();
+
+		System.out.println("The name of this user is: " + name);
+	}
 }
